15b154277946beb9f4f07f136af2b1f4ad5c069d
patgra1410-ub <114823108+patgra1410@users.noreply.github.com>
2025-03-24 00:11:38 +0100

    st test now prints out stderr

--- judge/judge.go
+++ judge/judge.go
@@ -3,6 +3,9 @@ package judge
 import (
 	"os"
 	"strings"
+
+	"fmt"
+	"github.com/fatih/color"
 )
 
 func Judge(inPath, ansPath, sampleID, command string, oiejqOptions *OiejqOptions) Verdict {
@@ -21,6 +24,11 @@ func Judge(inPath, ansPath, sampleID, command string, oiejqOptions *OiejqOptions
 	if err != nil || processInfo.Status != OK {
 		return Verdict{processInfo.Status, processInfo.TimeInSeconds, processInfo.MemoryInMegabytes, "", err}
 	}
+	if err == nil && len(processInfo.Stderr) > 0 { // Check if stderr was captured
+		fmt.Println(color.New(color.FgCyan).Sprint("-----Stderr-----"))
+    	fmt.Fprintln(os.Stderr, "Captured Stderr Output:\n"+string(processInfo.Stderr))
+	}
+
 
 	b, err := os.ReadFile(ansPath)
 	if err != nil {
